9efef31a14b3bacc74961d6d6b03bd5d7d1f7c24
aduforest <alexandreduforest@yahoo.com>
2024-09-08 19:55:42 -0700

    Minor fixes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <title>Next.js MongoDB App</title>
+        <title>Universal Collectibles App</title>
         <meta
           key="viewport"
           name="viewport"
@@ -15,16 +15,16 @@ const Layout = ({ children }) => {
         />
         <meta
           name="description"
-          content="nextjs-mongodb-app is a continously developed app built with Next.JS and MongoDB. This project goes further and attempts to integrate top features as seen in real-life apps."
+          content="Universal Collectibles (in dev)"
         />
-        <meta property="og:title" content="Next.js + MongoDB App" />
+        <meta property="og:title" content="Universal Collectibles App" />
         <meta
           property="og:description"
-          content="nextjs-mongodb-app is a continously developed app built with Next.JS and MongoDB. This project goes further and attempts to integrate top features as seen in real-life apps."
+          content="In development"
         />
         <meta
           property="og:image"
-          content="https://repository-images.githubusercontent.com/201392697/5d392300-eef3-11e9-8e20-53310193fbfd"
+          content="/images/main.jpg"
         />
       </Head>
       <Nav />
--- src/page-components/Index/Hero.jsx
+++ src/page-components/Index/Hero.jsx
@@ -1,17 +1,17 @@
-// import { ButtonLink } from '../../components/Button';
-// import { Container, Spacer, Wrapper } from '../../components/Layout';
-// import Link from 'next/link';
+import { ButtonLink } from '../../components/Button';
+import { Container, Spacer, Wrapper } from '../../components/Layout';
+import Link from 'next/link';
 import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <div>
+    <Wrapper>
       <h1 className={styles.title}>
         <span className={styles.nextjs}>Universal</span>
         <span className={styles.mongodb}>Collectibles</span>
         <span>App</span>
       </h1>
-      {/* <Container justifyContent="center" className={styles.buttons}>
+      <Container justifyContent="center" className={styles.buttons}>
         <Container>
           <Link passHref href="/feed">
             <ButtonLink className={styles.button}>Explore Feed</ButtonLink>
@@ -20,7 +20,7 @@ const Hero = () => {
         <Spacer axis="horizontal" size={1} />
         <Container>
           <ButtonLink
-            href="https://github.com/hoangvvo/nextjs-mongodb-app"
+            href="https://github.com/aduforest/tbd"
             type="secondary"
             className={styles.button}
           >
@@ -29,10 +29,9 @@ const Hero = () => {
         </Container>
       </Container>
       <p className={styles.subtitle}>
-        A Next.js and MongoDB web application, designed with simplicity for
-        learning and real-world applicability in mind.
-      </p> */}
-    </div>
+        Digital Sports Collectibles Reimagined
+      </p>
+    </Wrapper>
   );
 };
 
