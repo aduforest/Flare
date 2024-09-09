ad229273ef756642a3ebc66536378a6e0551b19f
aduforest <alexandreduforest@yahoo.com>
2024-09-08 21:15:09 -0700

    Create index.js

---
+++ backend/api/user/index.js
@@ -0,0 +1,103 @@
+// backend/api/user/index.js
+import { ValidateProps } from '@/api-lib/constants';
+import { findUserByUsername, updateUserById } from '@/api-lib/db';
+import { auths, validateBody } from '@/api-lib/middlewares';
+import { getMongoDb } from '@/api-lib/mongodb';
+import { ncOpts } from '@/api-lib/nc';
+import { slugUsername } from '@/lib/user';
+import { v2 as cloudinary } from 'cloudinary';
+import multer from 'multer';
+import nc from 'next-connect';
+
+const upload = multer({ dest: '/tmp' });
+const handler = nc(ncOpts);
+
+// Cloudinary config if applicable
+if (process.env.CLOUDINARY_URL) {
+  const {
+    hostname: cloud_name,
+    username: api_key,
+    password: api_secret,
+  } = new URL(process.env.CLOUDINARY_URL);
+
+  cloudinary.config({
+    cloud_name,
+    api_key,
+    api_secret,
+  });
+}
+
+// Middleware for authentication
+handler.use(...auths);
+
+// GET user route
+handler.get(async (req, res) => {
+  if (!req.user) return res.json({ user: null });
+  return res.json({ user: req.user });
+});
+
+// PATCH user route to update user data
+handler.patch(
+  upload.single('profilePicture'),
+  validateBody({
+    type: 'object',
+    properties: {
+      username: ValidateProps.user.username,
+      name: ValidateProps.user.name,
+      bio: ValidateProps.user.bio,
+    },
+    additionalProperties: true,
+  }),
+  async (req, res) => {
+    if (!req.user) {
+      req.status(401).end();
+      return;
+    }
+
+    const db = await getMongoDb();
+    let profilePicture;
+    
+    if (req.file) {
+      const image = await cloudinary.uploader.upload(req.file.path, {
+        width: 512,
+        height: 512,
+        crop: 'fill',
+      });
+      profilePicture = image.secure_url;
+    }
+    
+    const { name, bio } = req.body;
+    let username;
+
+    if (req.body.username) {
+      username = slugUsername(req.body.username);
+      if (
+        username !== req.user.username &&
+        (await findUserByUsername(db, username))
+      ) {
+        res
+          .status(403)
+          .json({ error: { message: 'The username has already been taken.' } });
+        return;
+      }
+    }
+
+    const user = await updateUserById(db, req.user._id, {
+      ...(username && { username }),
+      ...(name && { name }),
+      ...(typeof bio === 'string' && { bio }),
+      ...(profilePicture && { profilePicture }),
+    });
+
+    res.json({ user });
+  }
+);
+
+// Config for file uploads
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
