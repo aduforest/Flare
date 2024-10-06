fe4653c0f4ca8dae9a656d0b13edee6d9bbb22e5
aduforest <alexandreduforest@yahoo.com>
2024-10-06 12:18:58 -0700

    Email verification

--- api-lib/mail.js
+++ api-lib/mail.js
@@ -1,11 +1,14 @@
-// This project uses the nodemailer library to send email
-// However, it is recommended to switch over to dedicated email services
-// like Mailgun, AWS SES, etc.
 import nodemailer from 'nodemailer';
 
-const nodemailerConfig = process.env.NODEMAILER_CONFIG
-  ? JSON.parse(process.env.NODEMAILER_CONFIG)
-  : {};
+const nodemailerConfig = {
+  host: process.env.NODEMAILER_HOST,
+  port: process.env.NODEMAILER_PORT,
+  secure: process.env.NODEMAILER_SECURE === 'true', // Convert string to boolean
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
+  },
+};
 
 const transporter = nodemailer.createTransport(nodemailerConfig);
 
@@ -23,6 +26,5 @@ export async function sendMail({ from, to, subject, html }) {
 }
 
 export const CONFIG = {
-  // TODO: Replace with the email you want to use to send email
-  from: nodemailerConfig?.auth?.user,
+  from: process.env.NODEMAILER_USER,
 };
